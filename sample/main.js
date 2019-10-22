dc618bbbb217c8badc72b40f847dbcb046fd786b
Takashi <Takashi@gmail.com>
2019-10-22 13:56:19 +0900

    コンテキストメニューを表示させる

--- sample/main.js
+++ sample/main.js
@@ -13,11 +13,31 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({
+    'width': 500,
+    'height': 600,
+    'transparent': true,
+    'frame': false
+  });
   mainWindow.loadURL('file://' + __dirname + '/index.html');
-
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
+
+  var Tray = reqire('tray');
+  var appIcon = new Tray(__dirname + '/images/icon.png');
+
+  var contextMenu = Menu.buildFromTemplate([
+    {label: '選択メニュー1', type: 'radio'},
+    {label: '選択メニュー2', type: 'radio'},
+    {type: 'separator'},
+    {label: 'サブメニュー1'},
+    {label: 'サブメニュー2'}
+  ]};
+  {label: '終了', accelerator: 'Command*Q', click: function() { app.quit(); }}
+])};
+appIcon.setContextMenu(contextMenu);
+appIcon.setToolTip('This is sample.');
 });
+
 
