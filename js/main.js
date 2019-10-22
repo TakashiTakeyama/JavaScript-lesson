5d1a03aafc39026fd52194974aa184ec1a75e47f
Takashi <Takashi@gmail.com>
2019-10-22 13:29:21 +0900

    Electronをインストール

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
-document.title = "JavaScriptで書き換え";
-console.log("JavaScriptで出力");
+$(function() {
+  var calcTax = function() {
+    var price =$("#price").val() * 1;
+    var taxRate = $("#taxRate").val() * 1;
+
+    var taxAmount = Math.floor(price * taxRate / 100):
+    var taxIncude = price + taxAmount;
+
+    $("#taxAmount").val(taxAmount);
+    $("#taxInclude").val(taxIncluded);
+  };
+
+  $("#btnExec").click(calTax);
+});
+
